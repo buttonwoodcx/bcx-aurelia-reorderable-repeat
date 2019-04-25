f2bf6db35bc7c4d8a923b61f4217653a3edd6b66
Chunpeng Huo <huocp@me.com>
2019-04-25 14:58:11 +1000

    fix: fix rendering performance issue on large array of objects
    Use original ArrayRepeatStrategy for array of objects, because it does not have the issue on reordering ['a', 'a', 'b'].
    Cannot use ArrayRepeatStrategy to hander reordering 'a' and 'a', because the optimisation thinks the array does not change.
    
    closes #8

--- src/reorderable-repeat-strategy-locator.js
+++ src/reorderable-repeat-strategy-locator.js
@@ -1,10 +1,22 @@
 import {SimpleArrayRepeatStrategy} from './simple-array-repeat-strategy';
+import {ArrayRepeatStrategy} from 'aurelia-templating-resources';
 
 export class ReorderableRepeatStrategyLocator {
   constructor() {
     this.matchers = [];
     this.strategies = [];
-    this.addStrategy(items => items instanceof Array, new SimpleArrayRepeatStrategy());
+    this.addStrategy(items => {
+      if (items instanceof Array) {
+        // Simple strategy is only for array of primitive value
+        return items.length === 0 || typeof items[0] !== 'object';
+      }
+    }, new SimpleArrayRepeatStrategy());
+    this.addStrategy(items => {
+      if (items instanceof Array) {
+        // Normal strategy is only for array of non-primitive value
+        return items.length && typeof items[0] === 'object';
+      }
+    }, new ArrayRepeatStrategy());
   }
 
   addStrategy(matcher, strategy) {
